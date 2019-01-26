ed7136097132c0a184b2251f39cb620857788e41
DonTheChimera <donaldtbradfield@gmail.com>
2019-01-26 15:58:03 -0600

    used binding to make a few inputs that are dynamically displayed to the page

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,25 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = '';
-
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    },2000)
-   }
+  userName = '';
 
   ngOnInit() {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Server Name: ' + this.serverName;
   }
-  
+
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
-
 }
